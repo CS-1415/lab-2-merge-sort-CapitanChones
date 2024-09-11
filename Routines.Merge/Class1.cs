ed41f543cd6a990edd13b4b8402b771c2a168082
Amando Thomas <angelica.vieyra@gmail.com>
2024-09-11 14:55:30 -0700

    passed Tests

--- Routines.Merge/Class1.cs
+++ Routines.Merge/Class1.cs
@@ -9,15 +9,47 @@ public class Sort
         var result = new List<int>();
         var _a = a.ToList();
         var _b = b.ToList();
-
+//determines When Process is over
         while (_a.Count > 0 && _b.Count > 0)
         {
 
+                
             if (_a.Min() <= _b.Min())
             {
                 int low = _a.Min();
                 result.Add(low);
                 _a.Remove(low);
+
+                //Checks to see if a list is empty and deals with any remaining Values in the list.
+                if (_a.Count() == 0 || _b.Count() == 0) 
+                {
+                    if (_a.Count == 0)
+                    {
+                        while (_b.Count() > 0)
+                        {
+                            low = _b.Min();
+                            result.Add(low);
+                            _b.Remove(low);
+                        }
+                    }
+                    if (_b.Count == 0)
+                    {
+                        while (_a.Count() > 0)
+                        {
+                            low = _a.Min();
+                            result.Add(low);
+                            _a.Remove(low);
+                        }
+                    }
+                    return result.ToArray();
+                }
+            }
+            
+            if (_a.Min() >= _b.Min())
+            {
+                int low = _b.Min();
+                result.Add(low);
+                _b.Remove(low);
                 if (_a.Count() == 0 || _b.Count() == 0)
                 {
                     if (_a.Count == 0)
@@ -31,39 +63,11 @@ public class Sort
                     }
                     if (_b.Count == 0)
                     {
-                        while(_a.Count() > 0)
+                        while (_a.Count() > 0)
                         {
-                        low = _a.Min();
-                        result.Add(low);
-                        _a.Remove(low);
-                        }
-                    }
-                    return result.ToArray();
-                }
-            }
-            if (_a.Min() >= _b.Min())
-            {
-                int low = _b.Min();
-                result.Add(low);
-                _b.Remove(low);
-                if (_a.Count() == 0 || _b.Count() == 0)
-                {
-                     if (_a.Count == 0)
-                    {
-                        while (_b.Count() > 0)
-                        {
-                            low = _b.Min();
+                            low = _a.Min();
                             result.Add(low);
-                            _b.Remove(low);
-                        }
-                    }
-                    if (_b.Count == 0)
-                    {
-                        while(_a.Count() > 0)
-                        {
-                        low = _a.Min();
-                        result.Add(low);
-                        _a.Remove(low);
+                            _a.Remove(low);
                         }
                     }
                     return result.ToArray();
@@ -74,9 +78,13 @@ public class Sort
 
     }
 
-public static int[] SortViaMergesort(int[] set)
-{
-    return[0,0,0,0,0,0,00,0,0,0,0,0,0,0,0,0,0,0];
-}
+    public static int[] SortViaMergesort(int[] values)
+    {
+        int middle = values.Length / 2;
+        int[] firstHalf = values[0..middle];
+        int[] secondHalf = values[middle..values.Length];
+
+        return CombineSortedArrays(firstHalf, secondHalf);
+    }
 
 }
